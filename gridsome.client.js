1173621f86512cc61c45bd056e58f226b4971a62
Alistair Shepherd <alistair@accudio.com>
2020-03-18 20:21:43 +0000

    Removed redundant process.isClient check

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,23 +1,21 @@
 module.exports = function(api, options) {
-  if (process.isClient) {
-    const klaroConfig = options
-    if (klaroConfig.apps) {
-      klaroConfig.apps = klaroConfig.apps.map(function(app) {
-        return {
-          ...app,
-          callback: function(consent, app) {
-            document.dispatchEvent(new CustomEvent('consentUpdate', {
-              detail: {
-                app: app.name,
-                consent
-              }
-            }))
-          },
-        }
-      })
-    }
-
-    window.klaroConfig = klaroConfig
-    window.klaro = require('./klaro/klaro.js')
+  const klaroConfig = options
+  if (klaroConfig.apps) {
+    klaroConfig.apps = klaroConfig.apps.map(function(app) {
+      return {
+        ...app,
+        callback: function(consent, app) {
+          document.dispatchEvent(new CustomEvent('consentUpdate', {
+            detail: {
+              app: app.name,
+              consent
+            }
+          }))
+        },
+      }
+    })
   }
+
+  window.klaroConfig = klaroConfig
+  window.klaro = require('./klaro/klaro.js')
 }
